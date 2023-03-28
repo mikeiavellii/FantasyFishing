13bbc4f535ffc2a74bb40884dac102bfe5a91ade
Mike <mike.blackburn.541@gmail.com>
2023-03-28 16:28:49 -0400

    started shop component and minor other fixes

--- FantasyFishing/Models/FishingDAL.cs
+++ FantasyFishing/Models/FishingDAL.cs
@@ -13,9 +13,9 @@ namespace FantasyFishing.Models
 
             //Request
             HttpWebRequest request = WebRequest.CreateHttp(url);
-            request.Headers.Add("X-RapidAPI-Key", "671e579402mshe540f19948d59f0p12c4a1jsn85fd6c1964c4");
-            request.Headers.Add("X-RapidAPI-Host", "fish-species.p.rapidapi.com");
-			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            request.Headers.Add(Secret.xAPI, Secret.apiKey);
+            request.Headers.Add(Secret.apiHost, Secret.apiHostLink);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
             //Convert it to JSON
             StreamReader reader = new StreamReader(response.GetResponseStream());
